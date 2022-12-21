eed80a4757071aa3c324e85f6ae5faa6721e2936
lukemitch23 <70588025+lukemitch23@users.noreply.github.com>
2022-12-21 10:20:01 +0000

    proto 1

--- website/login.php
+++ website/login.php
@@ -1,29 +1,21 @@
+<!DOCTYPE html>
 <html lang="en">
     <head>
-        <meta charset="UTF-8">
-        <title>Login</title>
-        <link rel="stylesheet" href="indexstylesheet.css">
-        <link href="favicon.ico" rel="icon" type="image/x-icon" />
+         <meta charset="UTF-8">
+         <title>Login</title>
+         <link rel="stylesheet" href="stylesheet.css">
     </head>
     <body>
-        <div class="indexcontent">
-            <h1>Camera auction website</h1>
-            <h2>Welcome returning user</h2>
-            <break></break>
-            <break></break>
-            <div class="loginform">
-                <form action="login.php" method="post">
-                    <label for="username">Username:</label>
-                    <br>
-                    <input type="text" name="uname" id="uname">
-                    <br>
-                    <label for="password">Password:</label>
-                    <br>
-                    <input type="password" name="psswd" id="psswd">
-                    <br>
-                    <input type="submit" value="Login">
-                </form>
-            </div>
+        <h1>Camera auction site</h1>
+        <div class="content">
+            <h2>Login</h2>
+            <form action="login.php" method="post">
+                <label for="username">Username</label>
+                <input type="text" name="uname" id="username">
+                <label for="password">Password</label>
+                <input type="password" name="psswd" id="password">
+                <input type="submit" value="Login">
+            </form>
         </div>
     </body>
 </html>
@@ -32,33 +24,20 @@
 session_start();
 include 'db_connect.php';
 If($_POST){
-    if ($_POST['uname'] == "" or $_POST['psswd'] == ""){
-        echo "<div class='content'>
-                <br></br>
-                <h3>One or more fields are empty</h3>
-            </div>";
-    } else {
     if (isset($_POST['uname']) && isset($_POST['psswd'])) {
         $uname = $_POST['uname'];
         $psswd = $_POST['psswd'];
-        $sql = "SELECT * FROM users WHERE username = '$uname'";
+        $sql = "SELECT * FROM users WHERE username = '$uname' AND password = '$psswd'";
         $result = mysqli_query($link, $sql);
         if (mysqli_num_rows($result) > 0) {
-            $row = mysqli_fetch_assoc($result);
-            if(password_verify($psswd, $row['password'])){
-                $_SESSION['username'] = $uname;
-                echo "<h4> Welcome " . $_SESSION['username'] . "</h4>";
-                sleep(2);
-                header("Location: home.php");
-            } else{
-                echo "Details are incorrect please try again";
-            }
+            echo "Login successful";
+            $_SESSION['username'] = $uname;
+            echo "<h4> Welcome " . $_SESSION['username'] . "</h4>";
+            sleep(2);
+            header("Location: home.php");
         } else {
             echo "Details are incorrect please try again";
         }
     }
-    }
 }
 ?>
-
-
