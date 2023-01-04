10ff5e8cb472effe7c900ca85b19ec63503f90b8
lukemitch23 <70588025+lukemitch23@users.noreply.github.com>
2023-01-04 09:34:22 +0000

    Can't work out why it won't work so delete stuff

--- website/login.php
+++ website/login.php
@@ -31,34 +31,7 @@
 <?php
 session_start();
 include 'db_connect.php';
-If($_POST){
-    if ($_POST['uname'] == "" or $_POST['psswd'] == ""){
-        echo "<div class='content'>
-                <br></br>
-                <h3>One or more fields are empty</h3>
-            </div>";
-    } else {
-    if (isset($_POST['uname']) && isset($_POST['psswd'])) {
-        $uname = $_POST['uname'];
-        $psswd = $_POST['psswd'];
-        $sql = "SELECT * FROM users WHERE username = '$uname'";
-        $result = mysqli_query($link, $sql);
-        if (mysqli_num_rows($result) > 0) {
-            $row = mysqli_fetch_assoc($result);
-            if(password_verify($psswd, $row['password'])){
-                $_SESSION['username'] = $uname;
-                echo "<h4> Welcome " . $_SESSION['username'] . "</h4>";
-                sleep(2);
-                header("Location: home.php");
-            } else{
-                echo "Details are incorrect please try again";
-            }
-        } else {
-            echo "Details are incorrect please try again";
-        }
-    }
-    }
-}
+
 ?>
 
 
