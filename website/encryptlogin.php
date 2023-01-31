511678a576643ed3800bef0353db7603080db318
lukemitch23 <70588025+lukemitch23@users.noreply.github.com>
2023-01-31 21:42:59 +0000

    new key

--- website/encryptlogin.php
+++ website/encryptlogin.php
@@ -17,7 +17,7 @@ if(($uname == "") or ($psswd == "")){
     $iv_length = openssl_cipher_iv_length($ciphering);
     $options = 0;
     $encryption_iv = '1234567891011121';
-    $encryption_key = '123';
+    $encryption_key = '6927926';
     $encrypted_uname = openssl_encrypt($uname, $ciphering,
             $encryption_key, $options, $encryption_iv);
     $encrypted_psswd = openssl_encrypt($psswd, $ciphering,
