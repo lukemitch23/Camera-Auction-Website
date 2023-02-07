3d39836e504e812d38c1b0a1077cbf63c1fefe89
lukemitch23 <70588025+lukemitch23@users.noreply.github.com>
2023-02-07 11:57:48 +0000

    Updated seller messaging

--- website/messageseller.php
+++ website/messageseller.php
@@ -7,20 +7,19 @@ if (isset($_POST['message'])) {
     if (is_null($messagecontnt)) {
         echo "No message has been entered";
         header("Location: home.php");
-    } else {
-        echo "$messagecontnt";
-        $postowner = $_POST['postowner'];
-        $username = $_SESSION['username'];
-        $listingID = $_POST['listingID'];
-        $sql = "INSERT INTO messages (sendinguser, receivinguser, messagecontent) VALUES ('$username', '$postowner', '$messagecontnt')";
-        $result = mysqli_query($link, $sql);
-        if ($result) {
+        exit;
+    }
+    $postowner = $_POST['postowner'];
+    $username = $_SESSION['username'];
+    $listingID = $_POST['listingID'];
+    $sql = "INSERT INTO messages (sendinguser, receivinguser, messagecontent) VALUES ('$username', '$postowner', '$messagecontnt')";
+    $result = mysqli_query($link, $sql);
+    if ($result) {
         echo "Message sent";
         header("Location: view_listing.php?listingID=$listingID");
-        } else {
-            echo "Message not sent to database";
-        }
+        exit;
     }
+    echo "Message not sent to database";
 }
 
 
