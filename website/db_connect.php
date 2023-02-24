a2f0bd564b9568ede023e74ff82507ebbd4709f5
lukemitch23 <70588025+lukemitch23@users.noreply.github.com>
2023-02-24 20:07:32 +0000

    Created base files

---
+++ website/db_connect.php
@@ -0,0 +1,12 @@
+<?php
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'server');
+define('DB_PASSWORD', 'server123');
+define('DB_NAME', 'site');
+
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+ 
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+?>
